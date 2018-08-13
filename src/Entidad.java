dde70f0d17f0f1671b842d4d0b0f259d2fd02b04
ccruzm631 <ccruzm631@gmail.com>
2018-08-13 17:40:54 -0500

    Add complete class

--- src/Entidad.java
+++ src/Entidad.java
@@ -1,4 +1,18 @@
 
 public class Entidad {
+	private String nombre;
 
+	public Entidad(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
 }
