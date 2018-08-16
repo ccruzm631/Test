8528ba2be66a3472262fcb6947de74cd8c15e2d8
ccruzm631 <ccruzm631@gmail.com>
2018-08-16 10:52:30 -0500

    Add class

---
+++ src/Entidad.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * @author ccruzm
+ *
+ */
+public class Entidad {
+	public String nombre;
+
+	public Entidad(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	
+	
+	
+}
